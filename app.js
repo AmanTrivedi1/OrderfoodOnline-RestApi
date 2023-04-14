a075629dbf189f567d855039dfceea0d4929cbaa
Aman18012002 <amantrivedi598@gmail.com>
2023-04-14 09:02:07 +0530

    version one Api

---
+++ app.js
@@ -0,0 +1,64 @@
+import express, { urlencoded } from "express";
+import dotenv from "dotenv";
+import passport from "passport";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { errorMiddleware } from "./middlewares/errorMiddleware.js";
+import { connectPassport } from "./utils/Provider.js";
+import session from "express-session";
+
+const app = express();
+export default app;
+
+dotenv.config({
+  path: "./config/config.env",
+});
+
+//Using MIddleware
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+
+    cookie: {
+      secure: process.env.NODE_ENV === "development" ? false : true,
+      httpOnly: process.env.NODE_ENV === "development" ? false : true,
+      sameSite: process.env.NODE_ENV === "development" ? false : "none",
+    },
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+app.use(
+  urlencoded({
+    extended: true,
+  })
+);
+
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+app.use(passport.authenticate("session"));
+app.use(passport.initialize());
+app.use(passport.session());
+app.enable("trust proxy");
+
+connectPassport();
+
+// IMporting Routes
+import userRoutes from "./routes/users.js";
+import orderRoutes from "./routes/order.js";
+
+app.use("/api/v1", userRoutes);
+app.use("/api/v1", orderRoutes);
+
+// Using Error Middleware
+
+app.use(errorMiddleware);
---
+++ middlewares/auth.js
@@ -0,0 +1,17 @@
+import ErrorHandler from "../utils/ErrorHandler.js";
+
+export const isAuthenticated = (req, res, next) => {
+  const token = req.cookies["connect.sid"];
+  if (!token) {
+    return next(new ErrorHandler("Not Logged In", 401));
+  }
+  console.log(token);
+  next();
+};
+
+export const authorizeAdmin = (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return next(new ErrorHandler("Only Admin Allowed", 405));
+  }
+  next();
+};
